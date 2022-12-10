81e1c37a4dcc5ec3ffea72095bd121d174b66060
Subasish Mula <subasishmula@gmail.com>
2022-12-10 23:40:49 +0530

    initial commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
              your business.</p>
              <div className='hero-buttons'>
                <button className="--btn --btn-secondary">
-              <Link to="/dashboard">Free Trial 1 Month </Link>
+              <Link to="/register">Free Trial 1 Month </Link>
               </button>
              </div>
 
